955aa0f684958ea707d94b7875c10b41a0e6ffc1
IvanIvanovJS <sibsmartco@gmail.com>
2025-09-19 13:57:29 +0300

    fixed drop down button issue

--- components/gallery/FiltersMobileSheet.tsx
+++ components/gallery/FiltersMobileSheet.tsx
@@ -554,17 +554,7 @@ export default function FiltersMobileSheet({
                         exit={{ opacity: 0 }}
                         className="mobile-filters-overlay"
                         onClick={closeSheet}
-                        style={{
-                            position: 'fixed',
-                            top: 0,
-                            left: 0,
-                            right: 0,
-                            bottom: 0,
-                            width: '100vw',
-                            height: '100vh',
-                            background: 'rgba(0, 0, 0, 0.5)',
-                            zIndex: 9998
-                        }}
+
                     />
 
                     <motion.div
