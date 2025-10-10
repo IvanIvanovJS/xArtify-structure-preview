a60e1d01435004a3326265bfe8603dcf87535b21
IvanIvanovJS <sibsmartco@gmail.com>
2025-10-10 05:38:50 +0000

    update home.tsx

--- components/home/HomeHero.tsx
+++ components/home/HomeHero.tsx
@@ -32,7 +32,7 @@ export default function HomeHero() {
                                 onClick={() => setShowLogoutConfirm(true)}
                                 className="footer-link inline-flex items-center gap-2"
                             >
-                                <LogOut size={18} /> Тестов
+                                <LogOut size={18} /> Изход
                             </button>
                         ) : (
                             <Link href="/login" className="footer-link inline-flex items-center gap-2">
